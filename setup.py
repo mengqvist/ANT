5f1e53874e7e49d8ae1f8318d25af6b8231bf51f
Jordan Willis <jordan.r.willis@vanderbilt.edu>
2016-05-05 17:59:55 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin env python
+from distutils.core import setup
+
+setup(name='ANT',
+      version='0.1',
+      description='Ambiguous Nucleotide Tool (ANT), software for generating and evaluating degenerate codons for natural and expanded genetic codes.',
+      author='Martin Engqvist',
+      author_email='martin_engqvist@hotmail.com',
+      py_modules=['dna','protein','ANT','base_class','pyperclip','colcol'],
+      scripts=['ANT.py','ANT_GUI.py'])
